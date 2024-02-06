cae575f55f075c36d086f2f3971c6227e939f410
seunghee17 <victoria9981@swu.ac.kr>
2024-02-06 18:15:10 +0900

    TextField 학습

---
+++ Image_Text/TextField/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+//            val textValue = remember{
+//                mutableStateOf("")
+//            }
+            //구조 분해 기법
+            val (text, setValue) = remember{
+                mutableStateOf("")
+            }
+            val scaffoldState = remember{SnackbarHostState()}
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
+
+           Scaffold(
+               //이게 무엇?
+               snackbarHost = {SnackbarHost(scaffoldState)}
+           ){
+               Column(
+                   //화면에 꽉 채운다
+                   modifier = Modifier.fillMaxSize(),
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally,
+               ){
+                   TextField(
+                       //이 값은 동적으로 변하게 만들어야함
+                       value= text,
+                       onValueChange = setValue,
+                   )
+                   Button(onClick={
+                       keyboardController?.hide()
+                       //showSnackbar는 suspend함수이므로 코루틴 내에서 호출되어야함
+                       scope.launch {
+                           scaffoldState.showSnackbar("Hello $text")
+                       }
+                   }){
+                       Text("클릭")
+                   }
+               }
+           }
+        }
+    }
+}
